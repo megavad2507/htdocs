7d1b420a2e6f2d53fb2cd99659b9628424e74a1a
megavad2507 <54946556+megavad2507@users.noreply.github.com>
2019-12-06 20:05:44 +0400

    fixed icon vk

--- Web/login.php
+++ Web/login.php
@@ -28,7 +28,7 @@
         <h3>Войти</h3>
         <p>Войти через:</p>
         <a href="https://oauth.vk.com/authorize?client_id=7236626&display=page&redirect_uri=http://jeweler-from-mos.com/validation-form/authVK.php&response_type=code">
-            <img src="http://info-less.ru/article/6/resources/vk.png"/>
+            <img class="img_vk" src="http://info-less.ru/article/6/resources/vk.png"/>
         </a>
         <?php
             if($_SESSION['try']){
